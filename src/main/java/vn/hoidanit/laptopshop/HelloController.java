5392df692d7dbc410f2f889163a2cbb90d4205c8
Phi Long 14 <nlong1403@gmail.com>
2025-10-11 16:38:39 +0700

    vid 40: setup security

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,6 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello world with Hoi dan IT!";
+        return "Hello world update 1!";
     }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "user Page!";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "admin Page!";
+    }
+
 }
