020b1e967941ad9c49ba91d4e828773fcedf1eb7
Phi Long 14 <nlong1403@gmail.com>
2025-10-12 00:02:06 +0700

    vid 46: html and @Controller

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -10,24 +10,26 @@ import vn.hoidanit.laptopshop.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-// @Controller
-// public class UserController {
-
-//     @RequestMapping("/")
-//     public String getHomePage() {
-//         return "hello from controller";
-//     }
-@RestController
+@Controller
 public class UserController {
 
-    private UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
-    @GetMapping("/")
+    @RequestMapping("/")
     public String getHomePage() {
-        return this.userService.handleHello();
+        return "eric.html";
     }
+
+    // @RestController
+    // public class UserController {
+
+    // // dependency injection
+    // private UserService userService;
+
+    // public UserController(UserService userService) {
+    // this.userService = userService;
+    // }
+
+    // @GetMapping("/")
+    // public String getHomePage() {
+    // return this.userService.handleHello();
+    // }
 }
